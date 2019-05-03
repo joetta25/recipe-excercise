34887e613e572153171f922131e2bc814790e029
joetta25 <joetta.chasidy@gmail.com>
2019-05-02 20:28:56 -0400

    added script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var two = 2;
+var oneFourth = "1/4";
+
+function marinaraSauce(cookName){
+    console.log("Hey, " + cookName + "!");
+    console.log("Heat a medium-large saucepan over medium heat");
+    console.log("Add " + two + " " + oneFourth + " of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Adam");
